d2402bf5816795c41186d7feeefdf9381131b455
trtlbby <earllawrence.bacsain@unc.edu.ph>
2026-05-05 16:44:44 +0800

    feat(csv): save the scrape data to a csv file
    - remove print to console function

--- app/scrape.py
+++ app/scrape.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 url = "https://books.toscrape.com/"
 response = requests.get(url)
@@ -21,3 +22,20 @@ for book in books:
         stock_status = stock.text
         stock_stats = stock_status.strip()
     print(f"{title} - {price_tag} is {stock_stats}")
+
+with open("books.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Title", "Price", "Stock Status"])
+
+    for book in books:
+        if book.h3 and book.h3.a:
+            title = book.h3.a["title"]
+
+        price = book.find("p", class_="price_color")
+        if price:
+            price_tag = price.text
+        stock = book.find("p", class_="instock availability")
+        if stock:
+            stock_status = stock.text
+            stock_stats = stock_status.strip()
+        writer.writerow([title, price_tag, stock_stats])
