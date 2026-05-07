69d91e22140be432be8e03398fb62389e0e362cb
trtlbby <earllawrence.bacsain@unc.edu.ph>
2026-05-07 15:55:26 +0800

    scrape other site for testing

--- app/scrape.py
+++ app/scrape.py
@@ -2,27 +2,47 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-url = "https://books.toscrape.com/"
-response = requests.get(url)
+url = "https://www.gutenberg.org/ebooks/bookshelf/671"
 
+response = requests.get(url)
 print(response.status_code)
 
 soup = BeautifulSoup(response.text, "html.parser")
-books = soup.find_all("article", class_="product_pod")
+books = soup.find_all("li", class_="booklink")
 
-with open("books.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(["Title", "Price", "Stock Status"])
+for book in books:
+    title_tag = book.find("span", class_="title")
+    subtitle_tag = book.find("span", class_="subtitle")
+    download_tag = book.find("span", class_="extra")
+    link_tag = book.find("a", class_="link")
+
+    if not (title_tag and subtitle_tag and download_tag and link_tag):
+        continue
+
+    title = title_tag.get_text(strip=True)
+    subtitle = subtitle_tag.get_text(strip=True)
+    download = download_tag.get_text(strip=True)
+    link = link_tag.get("href", "")
+
+    print(f"Title: {title}, by: {subtitle} with a total {download} downloads. For more: {link}")
+    
+
+with open("books.csv", "w", newline="", encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Title", "Subtitle", "Downloads", "Link"])
 
     for book in books:
-        if book.h3 and book.h3.a:
-            title = book.h3.a["title"]
+        title_tag = book.find("span", class_="title")
+        subtitle_tag = book.find("span", class_="subtitle")
+        download_tag = book.find("span", class_="extra")
+        link_tag = book.find("a", class_="link")
 
-        price = book.find("p", class_="price_color")
-        if price:
-            price_tag = price.text
-        stock = book.find("p", class_="instock availability")
-        if stock:
-            stock_status = stock.text
-            stock_stats = stock_status.strip()
-        writer.writerow([title, price_tag, stock_stats])
+        if not (title_tag and subtitle_tag and download_tag and link_tag):
+            continue
+
+        title = title_tag.get_text(strip=True)
+        subtitle = subtitle_tag.get_text(strip=True)
+        download = download_tag.get_text(strip=True)
+        link = link_tag.get("href", "")
+
+        writer.writerow([title, subtitle, download, link])
