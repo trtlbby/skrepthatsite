046166af561d1a8fa78eb15075ae71275560b751
trtlbby <earllawrence.bacsain@unc.edu.ph>
2026-05-09 17:09:02 +0800

    feat: accept paginated url

--- app/scrape.py
+++ app/scrape.py
@@ -3,14 +3,26 @@ import requests
 import csv
 
 url = "https://www.gutenberg.org/ebooks/bookshelf/671"
+page = f"{url}?start_index="
 
 response = requests.get(url)
 print(response.status_code)
 
 soup = BeautifulSoup(response.text, "html.parser")
-books = soup.find_all("li", class_="booklink")
+books = []
 
-for book in books:
+for i in range(5):
+    paginated_url = url if i == 0 else f"{page}{i * 25 + 1}"
+    paginated_response = requests.get(paginated_url)
+    paginated_soup = BeautifulSoup(paginated_response.text, "html.parser")
+    start_index = i * 25 + 1
+    end_index = start_index + 24
+    page_index = f"{start_index}-{end_index}"
+
+    for book in paginated_soup.find_all("li", class_="booklink"):
+        books.append((page_index, book))
+
+for index, book in books:
     title_tag = book.find("span", class_="title")
     subtitle_tag = book.find("span", class_="subtitle")
     download_tag = book.find("span", class_="extra")
@@ -24,14 +36,14 @@ for book in books:
     download = download_tag.get_text(strip=True)
     link = link_tag.get("href", "")
 
-    print(f"Title: {title}, by: {subtitle} with a total {download} downloads. For more: {link}")
+    print(f"Index: {index}, Title: {title}, by: {subtitle} with a total {download} downloads. For more: {link}")
     
 
 with open("books.csv", "w", newline="", encoding="utf-8") as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["Title", "Subtitle", "Downloads", "Link"])
+    writer.writerow(["Index", "Title", "Subtitle", "Downloads", "Link"])
 
-    for book in books:
+    for index, book in books:
         title_tag = book.find("span", class_="title")
         subtitle_tag = book.find("span", class_="subtitle")
         download_tag = book.find("span", class_="extra")
@@ -45,4 +57,4 @@ with open("books.csv", "w", newline="", encoding="utf-8") as csvfile:
         download = download_tag.get_text(strip=True)
         link = link_tag.get("href", "")
 
-        writer.writerow([title, subtitle, download, link])
+        writer.writerow([index, title, subtitle, download, link])
